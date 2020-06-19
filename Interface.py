0ad7fa68d6d8d9c40638b96843cd9bebf89361ba
AlexDebug <65367567+AlexDebug@users.noreply.github.com>
2020-06-18 21:12:46 -0600

    Estructura basica echa

--- Interface.py
+++ Interface.py
@@ -2,11 +2,11 @@ from pygame.locals import *
 import pygame
 import sys 
 
-HEIGHT = 64
-WIDTH  = 64
+HEIGHT = 500
+WIDTH  = 500
 
 display = pygame.display.set_mode((HEIGHT, WIDTH))
-
+pygame.draw.rect(display, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
 while True:
     
     events = pygame.event.get()
@@ -14,3 +14,16 @@ while True:
     for event in events:
         if event.type == QUIT:
             sys.exit()
+
+   
+    
+    click, _, _ = pygame.mouse.get_pressed()
+    if click:
+        pos = pygame.mouse.get_pos()
+        pygame.draw.circle(display, (255,255,255), pos, 10)
+
+    keys = pygame.key.get_pressed()
+    if keys[K_SPACE]:
+        pygame.draw.rect(display, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
+
+    pygame.display.flip()
